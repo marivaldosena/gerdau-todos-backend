5f6a4003168180f331d66c8793c535984e8c2bc2
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-28 21:17:56 -0300

    Incluir CORS.

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,18 @@ from app.config import get_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+
 app = Flask(__name__)
 
 config_name = os.environ.get('APP_ENV')
 app.config.from_object(get_config(config_name))
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+@app.after_request
+def configurar_cors(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 from app.routes.tipos_routes import tipos_bp
